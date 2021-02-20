a422eb64f60068c7b9ceab5e56a1be264dd94870
jinwoo <jintakk6531@gmail.com>
2021-02-20 16:44:07 +0900

    2/20

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { ApolloServer } = require('apollo-server');
-
-const mongoose = require('mongoose');
-
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
+const mongoose = require('mongoose');
 const { MONGODB } = require('./config.js');
 
 const server = new ApolloServer({
