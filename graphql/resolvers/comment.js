2a2aacf4e8f1707478aa094412f43c6eb29ff843
jinwoo <jintakk6531@gmail.com>
2021-02-25 21:36:24 +0900

    2/25

--- graphql/resolvers/comment.js
+++ graphql/resolvers/comment.js
@@ -1,4 +1,6 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
+
+const checkAuth = require('../../util/check-auth');
 const Post = require('../../models/Post');
 
 module.exports = {
@@ -12,7 +14,7 @@ module.exports = {
           },
         });
       }
-      //호출
+
       const post = await Post.findById(postId);
 
       if (post) {
@@ -24,6 +26,23 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError('Post not found');
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      }
     },
   },
 };
